973204cf9fc3020dc3fd0c68a8391de323d8178e
root <root@ip-172-31-16-248.ec2.internal>
2021-01-16 15:08:15 +0000

    Setup and Configure S3 bucket

--- testing.py
+++ testing.py
@@ -1,27 +1,78 @@
 import boto3
 import click
-session = boto3.Session(profile_name='Waqar')
+from botocore.exceptions import ClientError
 
+session = boto3.Session(profile_name ='Waqar')
 s3 = session.resource('s3')
 
 @click.group()
 def cli():
-    "my static website to AWS"
+    "S3 all options:"
     pass
-    
-@cli.command('list-buckets')
-def list_buckets():
-    "List all s3 buckets"
+
+@cli.command('List_Buckets')
+def List_Buckets():
+    "List all buckets in profile"
     for bucket in s3.buckets.all():
         print(bucket)
 
-@cli.command('get_objects_from_bucket')
+@cli.command('List_objects')
 @click.argument('bucket')
-def get_objects_from_bucket(bucket):
-    "get objects from bucket"
-    for obj in s3.Bucket(bucket).ojects.all():
-        print (obj)
+def List_objects(bucket):
+    "List objects"
+    for obj in s3.Bucket(bucket).objects.all():
+        print(obj)
 
-    
+@cli.command('Setup_bucket_static Website')
+@click.argument('bucket')
+def Setup_bucket_static(bucket):
+    "Setup bucket(Create and configure S3)"
+    s3_bucket = None
+    try:    
+        s3_bucket = s3.create_bucket(
+            Bucket=bucket,
+            CreateBucketConfiguration={
+                'LocationConstraint': session.region_name
+            }
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+            s3_bucket = s3.Bucket(bucket)
+        else:
+            raise e
+
+    policy = """
+            {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "PublicReadGetObject",
+                    "Effect": "Allow",
+                    "Principal": "*",
+                    "Action": [
+                        "s3:GetObject"
+                    ],
+                    "Resource": [
+                        "arn:aws:s3:::%s/*"
+                    ]
+                }
+            ]
+        }
+        """ % s3_bucket.name
+    policy = policy.strip()
+    pol = s3_bucket.policy()
+    pol.put(Policy=policy)
+
+    ws = s3_bucket.Website()
+    ws.put(WebsiteConfiguration={
+        'ErrorDocument': {
+            'key': 'error.html'
+        },
+        'IndexDocument': {
+            'Suffix': 'index.html'
+        }
+    })
+
 if __name__ == '__main__':
     cli()
+
